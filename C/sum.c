8931eb079e3a337fdc86097eb20cc9e76f8f04c7
laibarashid746 <157144468+laibarashid746@users.noreply.github.com>
2024-03-03 08:28:43 +0500

    Add files via upload

---
+++ C/sum.c
@@ -0,0 +1,13 @@
+// Code here
+
+#include <stdio.h>
+int main() {
+  int a, b;
+  printf("Enter the value of a: ");
+  scanf("%d", &a);
+  printf("Enter the value of b: ");
+  scanf("%d", &b);
+  int sum = a + b;
+  printf("Sum is: %d", &sum);
+  return 0;
+}
