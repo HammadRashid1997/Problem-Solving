6c9f88aea132f06bd9e6c13648bd1fe6c982f48e
Muhammad Hammad Rashid <160396261+HammadRashid1997@users.noreply.github.com>
2024-03-03 12:37:01 +0500

    Add files via upload

---
+++ Python/diff.py
@@ -0,0 +1,4 @@
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+diff = a - b
+print("The difference is: ", diff)
---
+++ Python/logicaloperators.py
@@ -0,0 +1,9 @@
+a = 3.14
+b = 1.618
+c = 12
+d = 3.14
+print(a>b and c>a)
+print(b>c and d>a)
+print(b<c or d>a)
+print( not a==b)
+print(not a==d)
