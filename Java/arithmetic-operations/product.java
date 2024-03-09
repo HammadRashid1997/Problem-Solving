7f434e207b8db95f61cd03d27d4168c1791a7dd1
Muhammad Hammad Rashid <160396261+HammadRashid1997@users.noreply.github.com>
2024-03-09 10:19:45 +0500

    Create product.java

---
+++ Java/arithmetic-operations/product.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MultiplyTwoNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = input.nextInt();
+
+        int prod = num1 * num2;
+
+        System.out.println("The product of " + num1 + " and " + num2 + " is " + prod + ".");
+    }
+}
+
