ab7bdaf23da07f8c0b0d1d7ae69f8b33e0c2a951
HammadRashidDevsinc <hammad.muhammad@devsinc.com>
2025-07-15 16:30:32 +0500

    Update sum.py

--- Python/Recursion/sum.py
+++ Python/Recursion/sum.py
@@ -1,9 +1,11 @@
+# Function to add numbers till n
 def Sum(n):
     sum = 0
     if n == 0 or n == 1:
         return n;
     else:
         return sum + Sum(n - 1)
-        
+
+# Main starts here
 n = int(input("Enter the number n: "))
 print("The sum is: ", Sum(n))
