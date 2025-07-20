8bd8afa7d71b6fc4a81dc17f52c22d91e18e6bbd
HammadRashidDevsinc <hammad.muhammad@devsinc.com>
2025-07-20 09:22:36 +0500

    Update PrimeFactors.java

--- Java/PrimeFactors.java
+++ Java/PrimeFactors.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;     
 
 public class PrimeFactors {
-// main function
     public static void main(String[] args) {
       Scanner scanner = new Scanner(System.in);
       System.out.print("Enter a number: "); // user enters a number
