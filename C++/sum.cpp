928df238bee8e3ba3a58b36eaaf12bd39a630218
HammadRashidDevsinc <hammad.muhammad@devsinc.com>
2025-07-23 20:40:59 +0500

    Update sum.cpp

--- C++/sum.cpp
+++ C++/sum.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int sumOfTwoNumbers(int a, int b) {
+  return a + b;
+}
+
 int main() {
   int a, b;
   cout << "Enter the value of a: ";
   cin >> a;
   cout << "Enter the value of b: ";
   cin >> b;
-  int sum = a + b;
+  int sum = sumOfTwoNumbers(a, b)
   cout << "The sum is: " << sum << endl;
   return 0;
 }
